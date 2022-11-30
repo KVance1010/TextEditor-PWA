bdc7df1720aa1caf95b3988dec9ba74a7f944646
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-29 20:53:11 -0800

    program is mostly working

--- client/src-sw.js
+++ client/src-sw.js
@@ -8,42 +8,36 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),   
-  ],
+	cacheName: 'page-cache',
+	plugins: [
+		new CacheableResponsePlugin({
+			statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+			maxAgeSeconds: 30 * 24 * 60 * 60,
+		}),
+	],
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
+	urls: ['/index.html', '/'],
+	strategy: pageCache,
 });
 
 offlineFallback({
-  pageFallback: '/offline.html',
-
+	pageFallback: '/offline.html',
 });
-
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 registerRoute(
-    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-      cacheName: 'asset-cache',
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-      ],
-    })
-  );
-
-registerRoute();
-
-
+	({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+	new StaleWhileRevalidate({
+		cacheName: 'asset-cache',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200],
+			}),
+		],
+	})
+);
